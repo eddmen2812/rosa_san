b2e3893f8a1b6e870a32ddd4f18aee756470d377
Edison Mendoza <107630770+eddmen2812@users.noreply.github.com>
2025-02-16 11:00:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Contraseña predefinida
-const CONTRASENA = "2022";
+const CONTRASENA = "2023";
 
 // Variable para almacenar la contraseña ingresada
 let contrasenaIngresada = "";
@@ -68,7 +68,7 @@ function mostrarCarta() {
 
 // Función para actualizar el contador
 function actualizarContador() {
-    const fechaInicio = new Date('2022-10-20');
+    const fechaInicio = new Date('2023-10-20');
     const ahora = new Date();
 
     let anos = ahora.getFullYear() - fechaInicio.getFullYear();
